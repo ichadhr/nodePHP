2f641d2b95c75c8aa3fbb7d2df9dba63df33b715
ichadhr <ichadhr@gmail.com>
2015-08-10 04:39:43 +0700

    add mysql dump table change author => name in index.php

---
+++ nodetest.sql
@@ -0,0 +1,17 @@
+/*
+Begin database nodetest
+*/
+
+SET NAMES utf8;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+--  Table structure for `ci_sessions`
+-- ----------------------------
+DROP TABLE IF EXISTS `message`;
+CREATE TABLE `message` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(40) NOT NULL DEFAULT '-',
+  `message` varchar(500) NOT NULL DEFAULT '-',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
